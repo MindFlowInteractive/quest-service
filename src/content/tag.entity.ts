0e0ba4ca47bfa6698ba16b2f4418e450f4441b8e
Yusuf Falade <faladeyusuf54@gmail.com>
2025-10-04 15:36:56 +0100

    entities for the content module

---
+++ src/content/tag.entity.ts
@@ -0,0 +1,14 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToMany } from 'typeorm';
+import { Content } from './content.entity';
+
+@Entity('tags')
+export class Tag {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  name: string;
+
+  @ManyToMany(() => Content, (content) => content.tags)
+  contents: Content[];
+}
