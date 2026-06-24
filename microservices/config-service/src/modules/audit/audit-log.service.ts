57410a28b5c424cf956a6ce694629f86331cb783
crazyminerparody <wsholanky@gmail.com>
2026-06-24 19:37:57 +0100

    fix(config-service): fix tsconfig decorators and audit service import

--- microservices/config-service/src/modules/audit/audit-log.service.ts
+++ microservices/config-service/src/modules/audit/audit-log.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { AuditLog } from '../entities';
+import { AuditLog } from '../../entities';
 
 @Injectable()
 export class AuditLogService {
